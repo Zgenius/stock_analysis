0959bbd54da82a9928063e03e3b8075d229bc2bb
cair <tiancaizhang@sfmail.sf-express.com>
2024-12-27 07:41:31 +0800

    优化代码

--- src/manager/px_data_collection.py
+++ src/manager/px_data_collection.py
@@ -3,10 +3,9 @@ from dao.financial_report_brief import FinancialReportBrief
 import utils.stock_utils as su
 import utils.calculate_utils as cu
 from time import sleep
-from manager.session_manager import SessionManager
 
 # 获取所有股票编码
-report_briefs = SessionManager.query(FinancialReportBrief.symbol, FinancialReportBrief.name).distinct().order_by(FinancialReportBrief.symbol.asc()).all()
+report_briefs = FinancialReportBrief.select(FinancialReportBrief.symbol, FinancialReportBrief.name).distinct().order_by(FinancialReportBrief.symbol.asc()).all()
 
 for report_brief in report_briefs:
     print(report_brief.symbol)
